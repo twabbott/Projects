65b2d99c9542a526e9af965a96d8f5ca04470415
Tom Abbott <tabbott@dealersocket.com>
2018-12-17 17:10:49 -0700

    (not sure what this is, but WTH)

---
+++ DemoProjects/IntroToAspNetCore/HelloWorld/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Give a specific file for your default
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("start-page.html");
+            app.UseDefaultFiles(options);
+
+            app.UseStaticFiles();
+        }
+    }
+}

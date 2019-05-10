172c8d9357c40f527680566296a19d8e4d605d0e
Tom Abbott <tabbott@dealersocket.com>
2019-05-10 16:41:12 -0600

    Demo using async/await with Promise.all

---
+++ js-demo/promise-race/scripts/index.js
@@ -0,0 +1,38 @@
+import { writeLine } from './pageConsole.js';
+
+async function sleep(msTime) {
+  return new Promise(resolve => setTimeout(resolve, msTime));
+}
+
+async function someDay(name) {
+  // Sleep between 1 and 5 seconds
+  await sleep(4000 * Math.random() + 1000);
+
+  // Throw an exception 20% of the time
+  if (Math.random() <= .20) {
+    throw new Error(name);
+  }
+
+  // Otherwise return successfully
+  return name;
+}
+
+(async () => {
+  writeLine("Racing to see who wins...");
+
+  try {
+    const winner = await Promise.race([
+      someDay('red'),
+      someDay('green'),
+      someDay('blue'),
+      someDay('yellow'),
+      someDay('black'),
+      someDay('white'),
+    ]);
+
+    writeLine(`Aaaaand the winner is: ${winner}`);
+  } catch (err) {
+    writeLine(`OOPS! ${err.message} threw an exception!`);
+  }
+
+})();

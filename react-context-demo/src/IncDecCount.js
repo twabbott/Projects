e001b207e9cf669519fadb9d045871a93e5dd498
Tom Abbott <twabbott@outlook.com>
2022-12-06 10:31:41 -0600

    Use better pattern

--- react-context-demo/src/IncDecCount.js
+++ react-context-demo/src/IncDecCount.js
@@ -2,12 +2,15 @@ import { useContext } from "react";
 import CounterContext from "./CounterContext";
 
 export default function IncDecCount() {
-    const ctx = useContext(CounterContext);
+    const {
+        countIncremented,
+        countDecremented
+    } = useContext(CounterContext);
 
     return (
         <>
-            <button type="button" onClick={() => ctx.setCount(ctx.count + 1)}>+</button>
-            <button type="button" onClick={() => ctx.setCount(ctx.count - 1)}>-</button>
+            <button type="button" onClick={countIncremented}>+</button>
+            <button type="button" onClick={countDecremented}>-</button>
         </>
     )
 }
